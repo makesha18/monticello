3b810181a277c5fc7c633ba575f94640f27d4891
makesha18 <msmakeeva18@gmail.com>
2020-01-11 15:45:20 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+;
+
+(function() {
+        'use strict';
+
+        // Initialize and add the map
+        function initBaMap() {
+            // The location of mapCenter
+
+            let city = {
+                new - york: {
+                    coords: { lat: 40.6451594, lng: 74.0850837 },
+                }
+
+
+            }
+            let mapCenter = city.new - york.coords;
+
+            // The map, centered at mapCenter
+            let mapEl = document.getElementById('map');
+
+            let mapOptions = {
+                zoom: 15,
+                center: mapCenter,
+                disableDefaultUI: true,
+            };
+        })
